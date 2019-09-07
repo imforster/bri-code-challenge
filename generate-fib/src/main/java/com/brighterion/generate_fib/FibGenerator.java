5f5c1549f575f9baa60e51547029b9d25f116c3b
Ian Forster <ian.forster@gmail.com>
2019-09-07 15:49:52 -0700

    Implementation of FibGenerator.

---
+++ generate-fib/src/main/java/com/brighterion/generate_fib/FibGenerator.java
@@ -0,0 +1,83 @@
+/**
+ * Based on user input will generate the sequence of fibinacci numbers from 0..N where N is specified value.
+ */
+package com.brighterion.generate_fib;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Fib Sequence Generator
+ * Utilizes BigInteger to prevent integer rollover for large values of N.
+ * @author iforster
+ *
+ */
+public class FibGenerator {
+	
+	private static Scanner scan;
+
+	public FibGenerator() {
+		
+	}
+	
+	/**
+	 * Recursive fib sequence generator with mem array to prevent repeat calculations
+	 * @param n - number in sequence to generate
+	 * @return
+	 */
+    public BigInteger fib(int n) {
+    	BigInteger[] mem = new BigInteger[n+2]; // add two to avoid index error where n is 0.
+        Arrays.fill(mem, BigInteger.ONE.negate());
+        mem[0] = BigInteger.ZERO;
+        mem[1] = BigInteger.ONE;
+        return fib(n,mem);
+    }
+    /**
+     * Recursive function test to see if value has been calculated if so return it otherwise
+     * compute value for n.
+     * @param n - number in sequence to generate
+     * @param mem - array containing calculated values
+     * @return
+     */
+    public BigInteger fib(int n, BigInteger[] mem) {
+        if (mem[n].equals(BigInteger.ONE.negate())) {
+            mem[n] = fib(n-2,mem).add(fib(n-1,mem));
+        }
+        return mem[n];
+    }
+
+	/**
+	 * Main program
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		boolean done = false;
+		FibGenerator fibGen = new FibGenerator();
+		
+		while (!done) {
+			System.out.println("Please specify number of fibonacci numbers to generate (or <=0 to quit):");
+			scan = new Scanner(System.in);
+			try {
+				int num = scan.nextInt();
+				
+				if (num <= 0) {
+					done = true;
+				} else {
+					System.out.print(fibGen.fib(0));
+					for (int i=1; i < num; i++) {
+						System.out.print("," + fibGen.fib(i));
+
+					}
+					System.out.println("\n\n---------------------------------------------\n");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input not a number.");
+			}
+		}
+		System.out.println("bye.");
+	}
+
+}
---
+++ generate-fib/src/test/java/com/brighterion/generate_fib/FibGeneratorTest.java
@@ -0,0 +1,48 @@
+package com.brighterion.generate_fib;
+
+import java.math.*;
+
+import junit.framework.TestCase;
+
+public class FibGeneratorTest extends TestCase {
+	
+	FibGenerator fibGen = new FibGenerator();
+
+	public void testZeroCase() {
+		assertTrue(fibGen.fib(0).equals(BigInteger.ZERO));
+	}
+	
+	public void testOneCase() {
+		assertTrue(fibGen.fib(1).equals(BigInteger.ONE));
+	}
+	
+	public void testOneHundredCaseIsPositive() {
+		System.out.println(fibGen.fib(100) + " > 0");
+		assertTrue(fibGen.fib(100).compareTo(BigInteger.ZERO) == 1);
+	}
+	
+    // A utility method that returns true if x is perfect square 
+    static  boolean isPerfectSquare(BigInteger x) 
+    { 
+        double s = Math.sqrt(x.doubleValue());
+        return (s*s == x.doubleValue()); 
+    } 
+	
+    // Returns true if n is a Fibonacci Number, else false 
+    static boolean isFibonacci(BigInteger n) 
+    { 
+        // n is Fibonacci if one of 5*n*n + 4 or 5*n*n - 4 or both 
+        // is a perfect square 
+        return isPerfectSquare(n.multiply(n).multiply(new BigInteger("5")).add(new BigInteger("4"))) || 
+               isPerfectSquare(n.multiply(n).multiply(new BigInteger("5")).subtract(new BigInteger("4"))); 
+    } 
+	
+    // Verifies that first seventy five numbers are correct.
+	public void testFirstSeventyFive() {
+		for (int i=0; i < 75; i++) {
+			System.out.println(fibGen.fib(i));
+			assertTrue(isFibonacci(fibGen.fib(i)));
+		}
+	}
+
+}
